837b5033cb1358f18689087d476003c55e03106d
Joseph Johnson <jwjbadger@gmail.com>
2026-02-16 03:16:41 -0600

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,232 +1,31 @@
 use esp_idf_hal::{
-    gpio::{self, Pin, PinDriver, Pull},
-    peripheral::Peripheral,
-    peripherals::Peripherals,
-    task::notification::Notification,
-    timer::{self, TimerDriver},
+    delay::FreeRtos, gpio::{self, Pin, PinDriver, Pull}, peripheral::Peripheral, peripherals::Peripherals, task::notification::Notification, timer::{self, TimerDriver}
 };
 use esp_idf_sys::*;
-use std::num::NonZeroU32;
+use std::{collections::HashMap, num::NonZeroU32};
 
 const BITSET: NonZeroU32 = NonZeroU32::new(0xbeef).unwrap();
 
-#[derive(PartialEq, Clone, Copy)]
-enum State {
-    Message,
-    Accepted,
-    Rejected,
-    Waiting,
-}
+const BLANK: [bool; 24] = [
+        true, true, true, true, true, true, true, true,
+        true, true, true, true, true, true, true, true,
+        true, true, true, true, true, true, true, true,
+        ];
 
-#[rustfmt::skip]
-const MESSAGE: [[bool; 64]; 5] = [
-    [true, true, true, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, true, true, true, false, false, false, false,
-    true, false, false, false, false, false, false, false,
-    true, false, false, false, false, false, false, false,
-    true, false, false, false, false, false, false, false,
-    true, false, false, false, false, false, false, false,],
-
-    [true, true, true, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, true, true, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, false, false, false, true, false, false, false,
-    true, false, false, false, false, true, false, false,
-    true, false, false, false, false, true, false, false,],
-
-    [true, true, true, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, true, true, true, false, false, false, false,],
-
-    [true, true, true, true, true, true, true, true,
-    true, false, false, true, true, false, false, true,
-    true, false, false, true, true, false, false, true,
-    true, false, false, true, true, false, false, true,
-    true, false, false, true, true, false, false, true,
-    true, false, false, false, false, false, false, true,
-    true, false, false, false, false, false, false, true,
-    true, false, false, false, false, false, false, true,],
-
-    [true, true, true, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    false, false, true, true, false, false, false, false,
-    false, false, true, false, false, false, false, false,
-    false, false, true, false, false, false, false, false,
-    false, false, false, false, false, false, false, false,
-    false, false, true, false, false, false, false, false,],
-    ];
-
-#[rustfmt::skip]
-const WAITING: [bool; 64] = 
-    [true, false, true, false, false, false, false, false,
-    false, true, false, false, false, false, false, false,
-    false, true, false, false, false, false, false, false,
-    false, true, false, false, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, true, false, true, false, false, false, false,
-    true, false, true, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,];
-
-
-#[rustfmt::skip]
-const ACCEPTED: [[bool; 64]; 6] = [
-    [true, false, false, false, true, false, false, false,
-    false, true, false, true, false, false, false, false,
-    false, false, true, false, false, false, false, false,
-    false, false, true, false, false, false, false, false,
-    false, false, true, false, false, false, false, false,
-    false, false, true, false, false, false, false, false,
-    false, false, true, false, false, false, false, false,
-    false, false, true, false, false, false, false, false,],
-
-    [true, true, true, true, true, false, false, false,
-    false, false, true, false, false, false, false, false,
-    false, false, true, false, false, false, false, false,
-    false, false, true, false, false, false, false, false,
-    false, false, true, false, false, false, false, false,
-    false, false, true, false, false, false, false, false,
-    false, false, true, false, false, false, false, false,
-    true, true, true, true, true, false, false, false,],
-
-    [true, true, true, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, true, true, true, false, false, false, false,
-    true, false, false, false, false, false, false, false,
-    true, false, false, false, false, false, false, false,
-    true, false, false, false, false, false, false, false,
-    true, false, false, false, false, false, false, false,],
-
-    [true, true, true, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, true, true, true, false, false, false, false,
-    true, false, false, false, false, false, false, false,
-    true, false, false, false, false, false, false, false,
-    true, false, false, false, false, false, false, false,
-    true, false, false, false, false, false, false, false,],
-
-    [true, true, true, true, false, false, false, false,
-    true, true, false, false, false, false, false, false,
-    true, false, false, false, false, false, false, false,
-    true, true, true, false, false, false, false, false,
-    true, true, true, false, false, false, false, false,
-    true, false, false, false, false, false, false, false,
-    true, true, false, false, false, false, false, false,
-    true, true, true, true, false, false, false, false,],
-
-    [true, true, true, true, false, false, false, false,
-    true, true, false, false, false, false, false, false,
-    true, false, false, false, false, false, false, false,
-    true, true, true, false, false, false, false, false,
-    true, true, true, false, false, false, false, false,
-    true, false, false, false, false, false, false, false,
-    true, true, false, false, false, false, false, false,
-    true, true, true, true, false, false, false, false,],
-    ];
-
-
-#[rustfmt::skip]
-const REJECTED: [[bool; 64]; 8] = [
-        [true, true, true, true, true, false, false, false,
-        false, false, true, false, false, false, false, false,
-        false, false, true, false, false, false, false, false,
-        false, false, true, false, false, false, false, false,
-        false, false, true, false, false, false, false, false,
-        false, false, true, false, false, false, false, false,
-        false, false, true, false, false, false, false, false,
-        false, false, true, false, false, false, false, false,],
-
-        [true, true, true, true, false, false, false, false,
-        true, false, false, true, false, false, false, false,
-        true, false, false, true, false, false, false, false,
-        true, true, true, true, false, false, false, false,
-        true, false, false, true, false, false, false, false,
-        true, false, false, false, true, false, false, false,
-        true, false, false, false, false, true, false, false,
-        true, false, false, false, false, true, false, false,],
-
-        [true, false, false, false, true, false, false, false,
-        false, true, false, true, false, false, false, false,
-        false, false, true, false, false, false, false, false,
-        false, false, true, false, false, false, false, false,
-        false, false, true, false, false, false, false, false,
-        false, false, true, false, false, false, false, false,
-        false, false, true, false, false, false, false, false,
-        false, false, true, false, false, false, false, false,],
-
-        [false, true, true, false, false, false, false, false,
-        true, true, true, true, false, false, false, false,
-        true, false, false, true, false, false, false, false,
-        true, false, false, true, false, false, false, false,
-        true, true, true, true, false, false, false, false,
-        true, false, false, true, false, false, false, false,
-        true, false, false, true, false, false, false, false,
-        true, false, false, true, false, false, false, false,],
-
-        [true, true, true, true, false, false, false, false,
-        true, false, false, false, false, false, false, false,
-        true, false, false, false, false, false, false, false,
-        true, false, false, false, false, false, false, false,
-        true, false, true, true, false, false, false, false,
-        true, false, false, true, false, false, false, false,
-        true, false, false, true, false, false, false, false,
-        true, true, true, true, false, false, false, false,],
-
-        [false, true, true, false, false, false, false, false,
-        true, true, true, true, false, false, false, false,
-        true, false, false, true, false, false, false, false,
-        true, false, false, true, false, false, false, false,
-        true, true, true, true, false, false, false, false,
-        true, false, false, true, false, false, false, false,
-        true, false, false, true, false, false, false, false,
-        true, false, false, true, false, false, false, false,],
-
-        [true, true, true, true, true, false, false, false,
-        false, false, true, false, false, false, false, false,
-        false, false, true, false, false, false, false, false,
-        false, false, true, false, false, false, false, false,
-        false, false, true, false, false, false, false, false,
-        false, false, true, false, false, false, false, false,
-        false, false, true, false, false, false, false, false,
-        true, true, true, true, true, false, false, false,],
-
-        [true, false, false, false, false, true, false, false,
-        true, true, false, false, false, true, false, false,
-        true, false, true, false, false, true, false, false,
-        true, false, true, false, false, true, false, false,
-        true, false, false, true, false, true, false, false,
-        true, false, false, true, false, true, false, false,
-        true, false, false, false, true, true, false, false,
-        true, false, false, false, false, true, false, false,],
-
-    ];
-
-
-struct Charlieplex<'a> {
+struct Charlieplex {
     //pins: Vec<PinDriver<'a, gpio::AnyIOPin, gpio::InputOutput>>,
     pins: Vec<i32>,
     low: i32,
     high: i32,
-    reference: &'a [bool],
+    reference: Vec<bool>,
     layout: (usize, usize),
     index: usize,
 }
 
-impl<'a> Charlieplex<'a> {
+impl Charlieplex {
     fn new(
         pins: impl IntoIterator<Item = i32>,
-        reference: &'a [bool],
+        reference: Vec<bool>,
         layout: (impl Into<usize>, impl Into<usize>),
     ) -> Self {
         let pins = pins.into_iter().collect::<Vec<_>>();
@@ -268,7 +67,7 @@ impl<'a> Charlieplex<'a> {
         }
     }
 
-    fn reference(&mut self, reference: &'a [bool]) {
+    fn reference(&mut self, reference: Vec<bool>) {
         if reference.len() != self.layout.0 * self.layout.1 {
             panic!("Reference length does not match the layout dimensions.");
         }
@@ -303,6 +102,34 @@ impl<'a> Charlieplex<'a> {
     }
 }
 
+fn build_window(alphabet: &HashMap<char, u16>, msg: &[char]) -> [bool; 33] {
+    if msg.len() != 3 {
+        panic!("improperly sized message");
+    }
+
+    let msg_bit_coded = [alphabet.get(&msg[0]).unwrap(), alphabet.get(&msg[1]).unwrap(), alphabet.get(&msg[2]).unwrap()];
+
+    [
+        msg_bit_coded[0] & (1 << 8) != 0, msg_bit_coded[0] & (1 << 7) != 0, msg_bit_coded[0] & (1 << 6) != 0, false, msg_bit_coded[1] & (1 << 8) != 0, msg_bit_coded[1] & (1 << 7) != 0, msg_bit_coded[1] & (1 << 6) != 0, false, msg_bit_coded[2] & (1 << 8) != 0, msg_bit_coded[2] & (1 << 7) != 0, msg_bit_coded[2] & (1 << 6) != 0,   
+        msg_bit_coded[0] & (1 << 5) != 0, msg_bit_coded[0] & (1 << 4) != 0, msg_bit_coded[0] & (1 << 3) != 0, false, msg_bit_coded[1] & (1 << 5) != 0, msg_bit_coded[1] & (1 << 4) != 0, msg_bit_coded[1] & (1 << 3) != 0, false, msg_bit_coded[2] & (1 << 5) != 0, msg_bit_coded[2] & (1 << 4) != 0, msg_bit_coded[2] & (1 << 3) != 0,   
+        msg_bit_coded[0] & (1 << 2) != 0, msg_bit_coded[0] & (1 << 1) != 0, msg_bit_coded[0] & (1 << 0) != 0, false, msg_bit_coded[1] & (1 << 2) != 0, msg_bit_coded[1] & (1 << 1) != 0, msg_bit_coded[1] & (1 << 0) != 0, false, msg_bit_coded[2] & (1 << 2) != 0, msg_bit_coded[2] & (1 << 1) != 0, msg_bit_coded[2] & (1 << 0) != 0,   
+    ]
+}
+
+fn cut_window(data: &[bool; 33], offset: usize) -> [bool; 24] {
+    let mut out = [false; 24];
+
+    for row in 0..3 {
+        let src_start = row * 11 + offset;
+        let dst_start = row * 8;
+
+        out[dst_start..dst_start + 8]
+            .copy_from_slice(&data[src_start..src_start + 8]);
+    }
+
+    out
+}
+
 fn main() {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
@@ -310,6 +137,37 @@ fn main() {
     unsafe {
         esp_idf_sys::esp_task_wdt_deinit();
     }
+
+    let alphabet: HashMap<char, u16> = HashMap::from([
+        ('A', 0b010000101),
+        ('B', 0b110011111),
+        ('C', 0b111100111),
+        ('D', 0b110101110),
+        ('E', 0b111110111),
+        ('F', 0b111110100),
+        ('G', 0b110001111),
+        ('H', 0b101111101),
+        ('I', 0b010010010),
+        ('J', 0b011001111),
+        ('K', 0b101110101),
+        ('L', 0b100100111),
+        ('M', 0b111111101),
+        ('N', 0b111101101),
+        ('O', 0b111101111),
+        ('P', 0b111101100),
+        ('Q', 0b111101100),
+        ('R', 0b110111101),
+        ('S', 0b110111011),
+        ('T', 0b111010010),
+        ('U', 0b101101111),
+        ('V', 0b101000010),
+        ('W', 0b101111111),
+        ('X', 0b101010101),
+        ('Y', 0b101010010),
+        ('Z', 0b111010111),
+    ]);
+
+    let message = String::from("abcdefghijklmnopqrstuvwxyz").to_uppercase().chars().collect::<Vec<_>>();
 
     let peripherals = Peripherals::take().unwrap();
     let mut timer_driver = TimerDriver::new(
@@ -339,87 +197,37 @@ fn main() {
     timer_driver.enable(true).unwrap();
 
     let mut grid = Charlieplex::new(
-        [22, 21, 19, 18, 5, 4, 2, 15, 23, 13, 12, 14, 27, 26, 25, 33],
-        &MESSAGE[0],
-        (8usize, 8usize),
+        [15, 2, 4, 5, 18, 19, 21, 22, 12, 13, 23],
+        Vec::from(BLANK),
+        (8usize, 3usize),
     );
 
-    let mut high = PinDriver::output(peripherals.pins.gpio32).unwrap();
-    high.set_high().unwrap();
-
-
-    let mut acception = PinDriver::input(peripherals.pins.gpio35).unwrap();
-    let mut rejection = PinDriver::input(peripherals.pins.gpio34).unwrap();
-
+    let mut super_index: usize = 0;
     let mut index: usize = 0;
 
-    let mut state = State::Message;
+    let mut window = build_window(&alphabet, &message[super_index..super_index + 3]);
 
     loop {
-        let bitset = notification.wait(esp_idf_hal::delay::NON_BLOCK);
+        grid.reference(Vec::from(cut_window(&window, index)));
 
-        match bitset {
-            Some(BITSET) => {
-                let new_state = match &state {
-                    State::Message => {
-                        if index >= MESSAGE.len() - 1 {
-                            index = 0;
-                            grid.reference(&WAITING);
-                            Some(State::Waiting)
-                        } else {
-                            index += 1;
-                            grid.reference(&MESSAGE[index]);
-                            None
-                        }
-                    },
-                    State::Accepted => {
-                        index += 1;
-                        if index > ACCEPTED.len() - 1 {
-                            index = 0;
-                        }
-
-                        grid.reference(&ACCEPTED[index]);
-                        None
-                    },
-                    State::Rejected => {
-                        if index >= REJECTED.len() - 1 {
-                            index = 0;
-                            grid.reference(&MESSAGE[0]);
-                            Some(State::Message)
-                        } else {
-                            index += 1;
-                            grid.reference(&REJECTED[index]);
-                            None
-                        }
-                    },
-                    State::Waiting => { None },
-                };
-
-                if let Some(new_state) = new_state {
-                    state = new_state;
-                }
-            }
-            None => {
-                if state == State::Waiting {
-                    if acception.is_high() {
-                        state = State::Accepted;
-                        grid.reference(&ACCEPTED[0]);
-                        timer_driver.set_counter(0).unwrap();
-                        index = 0;
-                    } else if rejection.is_high() {
-                        state = State::Rejected;
-                        grid.reference(&REJECTED[0]);
-                        timer_driver.set_counter(0).unwrap();
-                        index = 0;
-                    }
-                }
-
-                grid.step();
-            }
-            _ => {
-                panic!("unexpected notification");
-            }
+        for _ in 0..100000 {
+            grid.step();
         }
-        //}
+
+        index += 1;
+
+        if index > 3 {
+            super_index += 1;
+            if super_index > message.len() - 3 {
+                for _ in 0..500000 {
+                    grid.step();
+                }
+
+                super_index = 0;
+            }
+
+            index = 0;
+            window = build_window(&alphabet, &message[super_index..super_index + 3]);
+        }
     }
 }
