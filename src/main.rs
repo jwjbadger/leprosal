1381c2b73b64b44321fe7187f7dc1387196ad5b3
Joseph Johnson <jwjbadger@gmail.com>
2026-03-26 21:48:04 -0500

    Add title to messages

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
+#![allow(incomplete_features)]
+#![feature(generic_const_exprs)]
+
 use esp_idf_hal::{
-    delay::FreeRtos, gpio::{self, Pin, PinDriver, Pull}, peripheral::Peripheral, peripherals::Peripherals, task::notification::Notification, timer::{self, TimerDriver}
+    gpio::{InterruptType, PinDriver, Pull},
+    peripherals::Peripherals,
 };
 use esp_idf_sys::*;
-use std::{collections::HashMap, num::NonZeroU32};
+use std::{
+    collections::HashMap,
+    sync::atomic::{AtomicU8, Ordering},
+};
 
-const BITSET: NonZeroU32 = NonZeroU32::new(0xbeef).unwrap();
+// Indicates which buttons have been selected
+static BUTTON_FLAGS: AtomicU8 = AtomicU8::new(0);
 
-const BLANK: [bool; 24] = [
-        true, true, true, true, true, true, true, true,
-        true, true, true, true, true, true, true, true,
-        true, true, true, true, true, true, true, true,
-        ];
-
+// A struct to display a message on an LED matrix using charlieplexing
 struct Charlieplex {
-    //pins: Vec<PinDriver<'a, gpio::AnyIOPin, gpio::InputOutput>>,
     pins: Vec<i32>,
     low: i32,
     high: i32,
@@ -95,6 +97,9 @@ impl Charlieplex {
         self.high = self.pins[(self.index / self.layout.0) + self.layout.0];
         self.low = self.pins[self.index % self.layout.0];
 
+        // Resetting a GPIO to high impedence mode is more technically accurate, but it takes too
+        // long, which breaks the illusion of persistance of vision. This method operates off of
+        // the LEDs being diodes and reverses the polarity for disabled pins.
         unsafe {
             gpio_set_level(self.high, 1);
             gpio_set_level(self.low, 0);
@@ -102,32 +107,156 @@ impl Charlieplex {
     }
 }
 
-fn build_window(alphabet: &HashMap<char, u16>, msg: &[char]) -> [bool; 33] {
-    if msg.len() != 3 {
-        panic!("improperly sized message");
-    }
-
-    let msg_bit_coded = [alphabet.get(&msg[0]).unwrap(), alphabet.get(&msg[1]).unwrap(), alphabet.get(&msg[2]).unwrap()];
-
-    [
-        msg_bit_coded[0] & (1 << 8) != 0, msg_bit_coded[0] & (1 << 7) != 0, msg_bit_coded[0] & (1 << 6) != 0, false, msg_bit_coded[1] & (1 << 8) != 0, msg_bit_coded[1] & (1 << 7) != 0, msg_bit_coded[1] & (1 << 6) != 0, false, msg_bit_coded[2] & (1 << 8) != 0, msg_bit_coded[2] & (1 << 7) != 0, msg_bit_coded[2] & (1 << 6) != 0,   
-        msg_bit_coded[0] & (1 << 5) != 0, msg_bit_coded[0] & (1 << 4) != 0, msg_bit_coded[0] & (1 << 3) != 0, false, msg_bit_coded[1] & (1 << 5) != 0, msg_bit_coded[1] & (1 << 4) != 0, msg_bit_coded[1] & (1 << 3) != 0, false, msg_bit_coded[2] & (1 << 5) != 0, msg_bit_coded[2] & (1 << 4) != 0, msg_bit_coded[2] & (1 << 3) != 0,   
-        msg_bit_coded[0] & (1 << 2) != 0, msg_bit_coded[0] & (1 << 1) != 0, msg_bit_coded[0] & (1 << 0) != 0, false, msg_bit_coded[1] & (1 << 2) != 0, msg_bit_coded[1] & (1 << 1) != 0, msg_bit_coded[1] & (1 << 0) != 0, false, msg_bit_coded[2] & (1 << 2) != 0, msg_bit_coded[2] & (1 << 1) != 0, msg_bit_coded[2] & (1 << 0) != 0,   
-    ]
+// Indicates whether we display the title of a message or its contents
+#[derive(PartialEq)]
+enum MessageState {
+    Playing,
+    Advertising,
 }
 
-fn cut_window(data: &[bool; 33], offset: usize) -> [bool; 24] {
-    let mut out = [false; 24];
+// A message player for an N x M LED matrix
+struct MessagePlayer<const N: usize, const M: usize>
+where
+    [(); N * (((M + N) & !N) + N)]:,
+{
+    messages: Vec<(String, String)>,
+    message_index: usize, // which message
+    window_index: usize, // which character
+    window_internal_offset: usize, // offset within character
+    state: MessageState,
+    window: [bool; N * (((M + N) & !N) + N)], // sized to 1 character past the width of the board
+    alphabet: HashMap<char, u16>,
+    layout: (usize, usize), // window layout (NOT BOARD)
+}
 
-    for row in 0..3 {
-        let src_start = row * 11 + offset;
-        let dst_start = row * 8;
-
-        out[dst_start..dst_start + 8]
-            .copy_from_slice(&data[src_start..src_start + 8]);
+impl<const N: usize, const M: usize> MessagePlayer<N, M>
+where
+    [(); N * (((M + N) & !N) + N)]:,
+{
+    fn new(alphabet: HashMap<char, u16>, messages: Vec<(String, String)>) -> Self {
+        Self {
+            messages,
+            message_index: 0,
+            window_index: 0,
+            window_internal_offset: 0,
+            state: MessageState::Advertising,
+            window: [false; N * (((M + N) & !N) + N)],
+            layout: (N, ((M + N) & !N) + N),
+            alphabet,
+        }
     }
 
-    out
+    fn rebuild_window(&mut self) {
+        let msg: &[char] = &match self.state {
+            MessageState::Advertising => &self.messages[self.message_index].0,
+            MessageState::Playing => &self.messages[self.message_index].1,
+        }
+        .to_uppercase()
+        .chars()
+        .collect::<Vec<_>>()[self.window_index..=self.window_index + M / (N + 1)];
+        // M / (N + 1) = number of characters that can be fit in a row
+
+        let mut msg_bit_coded = [0u16; N * (((M + N) & !N) + N)];
+
+        msg.iter().enumerate().for_each(|(i, char)| {
+            msg_bit_coded[i] = *self.alphabet.get(&char).unwrap();
+        });
+
+        for i in 0..self.layout.0 {
+            for j in 0..self.layout.1 {
+                self.window[i * self.layout.1 + j] = if j % (N + 1) == N {
+                    // space between characters
+                    false
+                } else {
+                    // j % (N + 1) gives (0, 1, 2)
+                    // N - 1 - (j % (N + 1)) gives the offset within the group (2, 1, 0)
+                    // (N - i - 1) * N shifts the base according to the row
+                    msg_bit_coded[j / (N + 1)] & (1 << ((N - 1 - (j % (N + 1))) + (N - i - 1) * N))
+                        != 0
+                }
+            }
+        }
+    }
+
+    fn step_window(&mut self) {
+        self.window_internal_offset += 1;
+
+        if self.window_internal_offset + M > self.layout.1 {
+            // We overflowed the board and need to regenerate our reference
+            self.window_internal_offset = 0;
+            self.window_index += 1;
+
+            if self.window_index + M / (N + 1) // + 1 for the additional character handled by >=
+                >= match self.state {
+                    MessageState::Advertising => &self.messages[self.message_index].0,
+                    MessageState::Playing => &self.messages[self.message_index].1,
+                }
+                .len()
+            {
+                // We overflowed the message and need to start over
+                if self.state == MessageState::Playing {
+                    self.state = MessageState::Advertising;
+                }
+
+                self.window_index = 0;
+                self.window_internal_offset = 0;
+            }
+
+            self.rebuild_window();
+        }
+    }
+
+    fn cut_window(&mut self) -> [bool; N * M] {
+        let mut out = [false; N * M];
+
+        for row in 0..N {
+            let src_start = row * self.layout.1 + self.window_internal_offset;
+            let dst_start = row * M;
+
+            out[dst_start..dst_start + M].copy_from_slice(&self.window[src_start..src_start + 8]);
+        }
+
+        out
+    }
+
+    fn toggle_play(&mut self) {
+        self.state = match self.state {
+            MessageState::Advertising => MessageState::Playing,
+            MessageState::Playing => MessageState::Advertising,
+        };
+
+        self.window_internal_offset = 0;
+        self.window_index = 0;
+        self.rebuild_window();
+    }
+
+    fn next(&mut self) {
+        self.message_index += 1;
+
+        if self.message_index >= self.messages.len() {
+            self.message_index = 0;
+        }
+
+        self.state = MessageState::Advertising;
+
+        self.window_internal_offset = 0;
+        self.window_index = 0;
+        self.rebuild_window();
+    }
+
+    fn previous(&mut self) {
+        if self.message_index <= 0 {
+            self.message_index = self.messages.len();
+        }
+
+        self.message_index -= 1;
+
+        self.state = MessageState::Advertising;
+
+        self.window_internal_offset = 0;
+        self.window_index = 0;
+        self.rebuild_window();
+    }
 }
 
 fn main() {
@@ -138,96 +267,137 @@ fn main() {
         esp_idf_sys::esp_task_wdt_deinit();
     }
 
-    let alphabet: HashMap<char, u16> = HashMap::from([
-        ('A', 0b010000101),
-        ('B', 0b110011111),
-        ('C', 0b111100111),
-        ('D', 0b110101110),
-        ('E', 0b111110111),
-        ('F', 0b111110100),
-        ('G', 0b110001111),
-        ('H', 0b101111101),
-        ('I', 0b010010010),
-        ('J', 0b011001111),
-        ('K', 0b101110101),
-        ('L', 0b100100111),
-        ('M', 0b111111101),
-        ('N', 0b111101101),
-        ('O', 0b111101111),
-        ('P', 0b111101100),
-        ('Q', 0b111101100),
-        ('R', 0b110111101),
-        ('S', 0b110111011),
-        ('T', 0b111010010),
-        ('U', 0b101101111),
-        ('V', 0b101000010),
-        ('W', 0b101111111),
-        ('X', 0b101010101),
-        ('Y', 0b101010010),
-        ('Z', 0b111010111),
-    ]);
-
-    let message = String::from("abcdefghijklmnopqrstuvwxyz").to_uppercase().chars().collect::<Vec<_>>();
-
     let peripherals = Peripherals::take().unwrap();
-    let mut timer_driver = TimerDriver::new(
-        peripherals.timer00,
-        &timer::config::Config {
-            auto_reload: true,
-            ..Default::default()
-        },
-    )
-    .unwrap();
 
-    let notification = Notification::new();
-    let notifier = notification.notifier();
+    let mut high = PinDriver::output(peripherals.pins.gpio33).unwrap();
+    high.set_high().unwrap();
 
-    timer_driver.set_alarm((timer_driver.tick_hz() * 3) / 4 ).unwrap();
+    let mut play = PinDriver::input(peripherals.pins.gpio26).unwrap();
+    play.set_pull(Pull::Down).unwrap();
+    play.set_interrupt_type(InterruptType::PosEdge).unwrap();
+
+    let mut next = PinDriver::input(peripherals.pins.gpio27).unwrap();
+    next.set_pull(Pull::Down).unwrap();
+    next.set_interrupt_type(InterruptType::PosEdge).unwrap();
+
+    let mut previous = PinDriver::input(peripherals.pins.gpio25).unwrap();
+    previous.set_pull(Pull::Down).unwrap();
+    previous.set_interrupt_type(InterruptType::PosEdge).unwrap();
 
     unsafe {
-        timer_driver
-            .subscribe(move || {
-                notifier.notify_and_yield(BITSET);
+        play.subscribe(|| {
+            BUTTON_FLAGS.fetch_or(0b001, Ordering::Relaxed);
+        })
+        .unwrap();
+
+        next.subscribe(|| {
+            BUTTON_FLAGS.fetch_or(0b010, Ordering::Relaxed);
+        })
+        .unwrap();
+
+        previous
+            .subscribe(|| {
+                BUTTON_FLAGS.fetch_or(0b100, Ordering::Relaxed);
             })
             .unwrap();
     }
 
-    timer_driver.enable_interrupt().unwrap();
-    timer_driver.enable_alarm(true).unwrap();
-    timer_driver.enable(true).unwrap();
+    play.enable_interrupt().unwrap();
+    next.enable_interrupt().unwrap();
+    previous.enable_interrupt().unwrap();
 
     let mut grid = Charlieplex::new(
         [15, 2, 4, 5, 18, 19, 21, 22, 12, 13, 23],
-        Vec::from(BLANK),
+        Vec::from([false; 24]),
         (8usize, 3usize),
     );
+    let mut message_player = MessagePlayer::<3, 8>::new(
+        HashMap::from([
+            ('A', 0b010000101),
+            ('B', 0b110011111),
+            ('C', 0b111100111),
+            ('D', 0b110101110),
+            ('E', 0b111110111),
+            ('F', 0b111110100),
+            ('G', 0b110001111),
+            ('H', 0b101111101),
+            ('I', 0b010010010),
+            ('J', 0b011001111),
+            ('K', 0b101110101),
+            ('L', 0b100100111),
+            ('M', 0b111111101),
+            ('N', 0b111101101),
+            ('O', 0b111101111),
+            ('P', 0b111101100),
+            ('Q', 0b111101100),
+            ('R', 0b110111101),
+            ('S', 0b110111011),
+            ('T', 0b111010010),
+            ('U', 0b101101111),
+            ('V', 0b101000010),
+            ('W', 0b101111111),
+            ('X', 0b101010101),
+            ('Y', 0b101010010),
+            ('Z', 0b111010111),
+            (' ', 0b000000000),
+            (':', 0b010000010),
+            ('.', 0b000000010),
+            (',', 0b000010110),
+            ('\'', 0b010010000),
+            ('0', 0b111101111),
+            ('1', 0b110010111),
+            ('2', 0b110010011),
+            ('3', 0b110011110),
+            ('4', 0b101111001),
+            ('5', 0b011010110),
+            ('6', 0b100111111),
+            ('7', 0b111001001),
+            ('8', 0b111111111),
+            ('9', 0b111111001),
+        ]),
+        vec![
+            ("title 1".to_string(), "message 1".to_string()),
+            ("title 2".to_string(), "message 2".to_string()),
+            ("title 3".to_string(), "message 3".to_string()),
+            ("title 4".to_string(), "message 4".to_string()),
+        ],
+    );
 
-    let mut super_index: usize = 0;
-    let mut index: usize = 0;
-
-    let mut window = build_window(&alphabet, &message[super_index..super_index + 3]);
+    message_player.rebuild_window();
+    let mut reference = message_player.cut_window();
 
     loop {
-        grid.reference(Vec::from(cut_window(&window, index)));
+        grid.reference(Vec::from(reference));
 
-        for _ in 0..100000 {
+        for _ in 0..150000 {
             grid.step();
         }
 
-        index += 1;
+        message_player.step_window();
+        reference = message_player.cut_window();
 
-        if index > 3 {
-            super_index += 1;
-            if super_index > message.len() - 3 {
-                for _ in 0..500000 {
-                    grid.step();
-                }
+        // Handle Input
+        let flags = BUTTON_FLAGS.swap(0, Ordering::Relaxed);
+        if flags == 0 {
+            continue;
+        }
 
-                super_index = 0;
-            }
+        if flags & 0b001 != 0 {
+            // toggle play
+            message_player.toggle_play();
+            play.enable_interrupt().unwrap();
+        }
 
-            index = 0;
-            window = build_window(&alphabet, &message[super_index..super_index + 3]);
+        if flags & 0b010 != 0 {
+            // next
+            message_player.next();
+            next.enable_interrupt().unwrap();
+        }
+
+        if flags & 0b100 != 0 {
+            // prev
+            message_player.previous();
+            previous.enable_interrupt().unwrap();
         }
     }
 }
